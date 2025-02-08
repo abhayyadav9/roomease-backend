865d38cfa05a89ff16b7abdaa334a001e50d39bd
Abhay yadav <yadavabhay8227.com>
2025-02-09 03:03:49 +0530

    Update CORS origin to production URL

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.use(cookieParser());
 
 
 app.use(cors({
-  origin: "http://localhost:5173", // Replace with your frontend URL
+  origin: "https://roomease-theta.vercel.app/", // Replace with your frontend URL
   credentials: true, // Allow cookies and authentication headers
 }));
 
