c2e4a0ff1dcbc466ccaeaa6b1072a5fec7ed6448
Abhay Yadav <162044736+abhayyadav9@users.noreply.github.com>
2025-02-20 22:01:01 +0530

    Update ownerRoute.js

--- routes/owner/ownerRoute.js
+++ routes/owner/ownerRoute.js
@@ -5,7 +5,7 @@ import isAuthenticated from "../../middleware/isAuthenticated.js";
 import { uploadSingle } from "../../middleware/multerSetup.js";
 const router= express.Router();
 
-router.get("/owner-details/:id",isAuthenticated,ownerDetail);
+router.get("/owner-details/:id",ownerDetail);
 router.put("/update-owner/:id",uploadSingle, updateOwner);
 
 
