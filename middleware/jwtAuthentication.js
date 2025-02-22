a6706a59689dfd63d20866b402bae88a23240ac2
Abhay Yadav <162044736+abhayyadav9@users.noreply.github.com>
2025-02-22 14:56:30 +0530

    Update jwtAuthentication.js

--- middleware/jwtAuthentication.js
+++ middleware/jwtAuthentication.js
@@ -1,14 +1,12 @@
-import jwt from 'jsonwebtoken';
+import jwt from "jsonwebtoken";
 
 export const generateTokenAndSetCookies = (userId, res) => {
-  const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
-    expiresIn: "7d",
-  });
+  const token = jwt.sign({ userId }, process.env.JWT_SECRET, { expiresIn: "7d" });
 
-  res.cookie('token', token, {
-    httpOnly: true, // Corrected typo
-    secure: process.env.NODE_ENV === 'production', // Use secure cookies in production
-    sameSite: "strict", // Prevent CSRF attacks
+  res.cookie("token", token, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production", // Secure only in production
+    sameSite: "None", // Important for cross-origin requests
     maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
   });
 
